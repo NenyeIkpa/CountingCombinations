f3e8e76e26ee874160021d43b467c3deb0164073
NenyeIkpa <nenyeikpa@gmail.com>
2022-05-10 14:07:12 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,39 @@
+fun Main() {
+
+}
+
+class CountingCombinations {
+    /*
+    Task
+Write a function that counts how many different ways you can make change for an amount of money given an array of coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2:
+
+1+1+1+1, 1+1+2, 2+2.
+The order of coins does not matter:
+
+1+1+2 == 2+1+1
+Also, assume that you have an infinite ammount of coins.
+
+Your function should take an amount to change and an array of unique denominations for the coins.
+
+Specification
+Challenge.countChange(money, coins)
+Parameters
+money: Int - Number to make change for
+
+coins: Array<Int> - Array of denominations
+
+Return Value
+Int - Number of ways change can be made
+
+Examples
+money	coins	Return Value
+4	    [1,2]	    3
+10  	[5,2,3]	    4
+11	    [5,7]	    0
+     */
+
+    fun countChange(money: Int, coins: ArrayList<Int>): Int {
+
+        return 0
+    }
+}
